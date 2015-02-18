3c1114b6903cbcc5d5dddd397449c73febe709e3
IngrainTea <jsfdiffp@gmail.com>
2015-02-18 12:45:56 +0300

    Task 2 complete

--- src/com/vitebsk/scala/ExPage159ClassEx.scala
+++ src/com/vitebsk/scala/ExPage159ClassEx.scala
@@ -8,15 +8,24 @@ object ExPage159ClassEx extends App {
   class Dimention(var height: Int, var width: Int)
 
   val theBox1 = new Dimention(5,7)
-  println("height = " + theBox1.height + " and width = " + theBox1.width)
+//  println("height = " + theBox1.height + " and width = " + theBox1.width)
 
   theBox1.height = 10
-  println("height = " + theBox1.height + " and width = " + theBox1.width)
+//  println("height = " + theBox1.height + " and width = " + theBox1.width)
 
   theBox1.width = 19
-  println("height = " + theBox1.height + " and width = " + theBox1.width)
+//  println("height = " + theBox1.height + " and width = " + theBox1.width)
 
   // Task 2
+  class Info(val name: String, var description: String)
+
+  val info1 = new Info("stuff", "Somebody")
+  println("The name = " + info1.name)
+  println("The description = " + info1.description)
+  info1.description = "Changed description"
+  println("The another description = " + info1.description)
+
+  
 
 
 
